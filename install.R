477ba045660bc1e7e3540016abaf99a8fda27def
Xun Li <xunli@uchicago.edu>
2019-10-10 23:47:17 -0700

    update v0.0.3 tutorial

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages('wkb')
 install.packages('sf')
 install.packages('sp')
-install.packages("https://github.com/lixun910/rgeoda/archive/0.0.1.tar.gz")
+install.packages("https://github.com/lixun910/rgeoda/releases/download/0.0.3/rgeoda_0.0.3.tar.gz")
